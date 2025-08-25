fe70bf856b96646a3dc2afdda0f4e58cc0c8f8a6
Pedro Amorim <amrm.developer@gmail.com>
2025-08-25 20:31:43 -0300

    getMovie controller

--- internal/interface/controller/movie_controller.go
+++ internal/interface/controller/movie_controller.go
@@ -1,1 +1,22 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type (
+	MovieController struct {}
+
+	GetMovieRequest struct {
+	Name string `json:"name" binding:"required"`
+	}
+)
+
+func (mc *MovieController) getMovie(c *gin.Context) {
+	var req GetMovieRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"erro, payload inválido": err.Error()})
+		return
+	}
+}
